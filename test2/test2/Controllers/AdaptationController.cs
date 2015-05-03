5e45c788fbc989b70fc2cd9dcf0c7260ca69e36b
sprodan <sprodan8@gmail.com>
2015-05-03 16:04:07 +0300

    Страница Компании

---
+++ test2/test2/Controllers/AdaptationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace test2.Controllers
+{
+    public class AdaptationController : Controller
+    {
+        // GET: Adaptation
+        public ActionResult Adaptation()
+        {
+            return View();
+        }
+    }
+}
