2ccb99a1fc2f5eba1df5f6212d697acec564ce96
sprodan <sprodan8@gmail.com>
2015-05-10 13:57:26 +0300

    Company get problem

--- test2/test2/Models/CompanyViewModels.cs
+++ test2/test2/Models/CompanyViewModels.cs
@@ -20,7 +20,11 @@ namespace test2.Models
         [Display(Name = "Почта компании")]
         public string Mail { get; set; }
 
-        public ICollection<GroupViewModel> Groups =>  new List<GroupViewModel>
+        public ICollection<GroupViewModel> Groups
+        {
+            get
+            {
+                return new List<GroupViewModel>
         {
             new GroupViewModel {Name = "Компания 1", Id = 1},
             new GroupViewModel { Name = "Компания 2", Id = 2},
@@ -29,26 +33,42 @@ namespace test2.Models
             new GroupViewModel { Name = "Компания 5", Id = 5},
 
         };
+            }
+        } 
 
-        public IEnumerable<PositionViewModel> Positions => new List<PositionViewModel>
+        public IEnumerable<PositionViewModel> Positions
         {
-            new PositionViewModel {Name = ".net Dev", UsersCount = 10, Id = 1},
-            new PositionViewModel {Name = "php Dev", UsersCount = 12, Id = 2},
-            new PositionViewModel {Name = "front-end dev", UsersCount = 30, Id = 3},
-            new PositionViewModel {Name = "designer", UsersCount = 5, Id = 4},
+            get
+            {
+                return new List<PositionViewModel>
+                {
+                    new PositionViewModel {Name = ".net Dev", UsersCount = 10, Id = 1},
+                    new PositionViewModel {Name = "php Dev", UsersCount = 12, Id = 2},
+                    new PositionViewModel {Name = "front-end dev", UsersCount = 30, Id = 3},
+                    new PositionViewModel {Name = "designer", UsersCount = 5, Id = 4},
 
-        };
+                };
+            }
+        }
 
 
-        public IEnumerable<EmployeeViewModel> Employees => new List<EmployeeViewModel>
+
+        public IEnumerable<EmployeeViewModel> Employees
         {
-            new EmployeeViewModel { Name = "Иванов Иван", IdGroup = 1, IdPosition = 1, TestPassed = true},
-            new EmployeeViewModel { Name = "Николаев Николай", IdGroup = 1, IdPosition = 2},
-            new EmployeeViewModel { Name = "Евгений Евгеньевич", IdGroup = 1, IdPosition = 3, TestPassed = true},
-            new EmployeeViewModel { Name = "Светлана Сватовна", IdGroup = 1, IdPosition = 4, TestPassed = true},
-            new EmployeeViewModel { Name = "Денис Денисов", IdGroup = 1, IdPosition = 1},
-            new EmployeeViewModel { Name = "Анна Ановна", IdGroup = 2, IdPosition = 2},
-        }; 
+            get
+            {
+                return new List<EmployeeViewModel>
+                {
+                    new EmployeeViewModel {Name = "Иванов Иван", IdGroup = 1, IdPosition = 1, TestPassed = true},
+                    new EmployeeViewModel {Name = "Николаев Николай", IdGroup = 1, IdPosition = 2},
+                    new EmployeeViewModel {Name = "Евгений Евгеньевич", IdGroup = 1, IdPosition = 3, TestPassed = true},
+                    new EmployeeViewModel {Name = "Светлана Сватовна", IdGroup = 1, IdPosition = 4, TestPassed = true},
+                    new EmployeeViewModel {Name = "Денис Денисов", IdGroup = 1, IdPosition = 1},
+                    new EmployeeViewModel {Name = "Анна Ановна", IdGroup = 2, IdPosition = 2},
+                };
+            }
+        }
+
 
         #region TypeConverter
 
